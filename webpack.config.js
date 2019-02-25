3e149f44d55625c7ff9463decf9287f64c629d67
Kenneth Trott <kentrottjr@gmail.com>
2019-02-24 16:57:00 -0800

    Begin initial file configuration
    Webpack, babel, and react configured.

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  entry: __dirname + '/client/src/index.jsx',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      }
+    ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/client/dist',
+    publicPath: '/'
+  },
+  devServer: {
+    contentBase: './client/dist',
+  },
+};
