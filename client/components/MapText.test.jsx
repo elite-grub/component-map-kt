d032da6d435092d21956369aed173aa0a357154e
Kenneth Trott <kentrottjr@gmail.com>
2019-02-27 11:11:27 -0800

    Update test configuration

--- client/components/MapText.test.jsx
+++ client/components/MapText.test.jsx
@@ -1,29 +1,13 @@
 import React from 'react';
-import { shallow, mount, render } from 'enzyme';
-
-import { configure } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import MapText from './MapText';
-import Adapter from 'enzyme-adapter-react-16';
-
-configure({ adapter: new Adapter() });
 
 function setup() {
-  const props = {
-    imgPath: 'some/image/path/to/a/mock/image',
-  };
   const wrapper = shallow(<MapText />);
   return { wrapper };
 }
 
 describe('MapText Test Suite', () => {
-  // it('should render without throwing an error', function() {
-  //   expect(shallow(<MapText />).contains(<div className="map-text"></div>)).toBe(true);
-  // });
-
-  // it('should render to static HTML', function() {
-  //   expect(render(<MapText />).text()).toEqual();
-  // });
-  
   it('Should have a ul', () => {
     const { wrapper } = setup();
     expect(wrapper.find('ul').exists()).toBe(true);
