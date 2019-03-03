d4d2b1c8db7adb8f28efaec2b14966810bd2e662
Kenneth Trott <kentrottjr@gmail.com>
2019-03-02 19:39:54 -0800

    Refactor map component for proxy

--- server/server.js
+++ server/server.js
@@ -7,9 +7,19 @@ const app = express();
 const port = 3333;
 
 app.use(bodyParser.json());
-// app.use(cors());
+
 // app.use(express.static(path.join(__dirname, './client/dist')));
 app.use(express.static('./client/dist'));
+// app.use(cors());
+
+app.use((req, res, next) => {
+  // Website you wish to allow to connect
+  res.header("Access-Control-Allow-Origin", "*");
+  // Request headers you wish to allow
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 app.get('/restaurant/:id', (req, res) => {
   selectRestaurant(Number(req.params.id))
